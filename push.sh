6184351ad8f82d91e8e9a696d40254aa0841bdba
Glowman554 <glowman434@gmail.com>
2022-02-20 14:52:33 +0100

    some things

---
+++ push.sh
@@ -0,0 +1,12 @@
+function push_dir {
+	(
+		cd $1
+		echo "Pushing $1"
+		git push origin HEAD:main || echo "Failed to push $1"
+	)
+}
+
+push_dir FoxOS-kernel
+push_dir FoxOS-programs/libc
+push_dir FoxOS-programs
+push_dir .
