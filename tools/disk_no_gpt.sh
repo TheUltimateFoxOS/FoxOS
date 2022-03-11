90e6535ce755dacc0ec12ac4666148de24b279b9
MoritzSchittenhelm <78039560+JartC0ding@users.noreply.github.com>
2022-03-11 12:08:33 +0100

    some fixes

--- tools/disk_no_gpt.sh
+++ tools/disk_no_gpt.sh
@@ -2,11 +2,15 @@
 
 bash tools/disk_generic.sh disk_root
 
-dd if=/dev/zero of=foxos.img bs=512 count=93750
+dd if=/dev/zero of=/root/foxos.img bs=512 count=93750 status=progress
 
-mkfs.vfat -F 32 foxos.img
+mkfs.vfat -F 32 /root/foxos.img
 
-mcopy -s -i foxos.img disk_root/* ::
+mcopy -s -i /root/foxos.img disk_root/* ::
 
-dd if=/dev/zero of=foxos2.img bs=512 count=93750
-mkfs.vfat -F 32 foxos2.img
+dd if=/dev/zero of=/root/foxos2.img bs=512 count=93750 status=progress
+mkfs.vfat -F 32 /root/foxos2.img
+
+echo Copying image files...
+mv /root/foxos.img ./
+mv /root/foxos2.img ./
